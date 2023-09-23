4d10d263d6c3b4a15edd9ecaac01b4c765244fa6
hgwtra <huong.mai198@gmail.com>
2023-09-23 16:02:38 +0300

    math captcha done

--- src/app/captcha/math/math.component.ts
+++ src/app/captcha/math/math.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 import {
   FormGroup,
   FormControl,
@@ -12,13 +12,26 @@ import {
   styleUrls: ['./math.component.css'], 
 })
 export class MathComponent  {
+  //Canvas
+  @ViewChild('canvas') canvas: ElementRef;
+
+  mathValidated: boolean | null = null;  // true = correct, false = incorrect, null = not yet validated
+
   mathFormGroup: FormGroup;
+  //Form validation
   constructor() {
     this.initForm();
   }
+
+  //randomize a number
   randomNumber = (min = 1, max = 10) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
+
+  operators = ["+", "-", "*"];
+
+  randomOperator = () => { return this.operators[Math.floor(Math.random() * this.operators.length)]; };
+
   initForm() {
     this.mathFormGroup = new FormGroup(
       {
@@ -28,17 +41,81 @@ export class MathComponent  {
         secondNumber: new FormControl(this.randomNumber(), [
           Validators.required
         ]),
-        answer: new FormControl("")
+        operator: new FormControl(this.randomOperator(), [
+          Validators.required
+        ]),
+        answer: new FormControl("", [
+          //Validators.maxLength(6)
+        ])
       },
       [this.answerValidator]
     );
   }
+
   answerValidator(form: AbstractControl) {
-    console.log(form.value);
-    const { firstNumber, secondNumber, answer } = form.value;
-    if (+answer === parseInt(firstNumber) + parseInt(secondNumber)) {
-      return null;
+    const { firstNumber, secondNumber, operator, answer } = form.value;
+
+    let result;
+    switch (operator) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+        default:
+            return { math: true }; // return an error if the operator is not recognized
+    }
+
+    if (+answer === result) {
+        return null;
     }
     return { math: true };
+}
+
+preventEnter(event: KeyboardEvent): void {
+  if (event.key === 'Enter') {
+    event.preventDefault();
   }
+}
+
+  ngAfterViewInit() {
+    this.drawEquation();
+  }
+
+  drawEquation() {
+    const ctx: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d');
+    // Clear the entire canvas
+    ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    ctx.font = '35px QuickSand';
+    ctx.fillText(`${this.mathFormGroup.get('firstNumber').value} ${this.mathFormGroup.get('operator').value} ${this.mathFormGroup.get('secondNumber').value}`, 10, 30);
+}
+  
+onSubmit() {
+  if (this.mathFormGroup.valid && (!this.mathFormGroup.errors || !this.mathFormGroup.errors['math'])) {
+    this.mathValidated = true;
+    this.mathFormGroup.get('answer').disable();
+  } else if (this.mathFormGroup.errors && this.mathFormGroup.errors['math']) {
+    this.mathValidated = false;
+    this.mathFormGroup.get('answer').disable(); 
+  }  
+}
+
+
+retry() {
+  this.mathValidated = null;
+  this.onRefreshButtonClicked();
+  this.mathFormGroup.get('answer').enable()
+}
+
+onRefreshButtonClicked() {
+  this.mathValidated = null;
+  this.initForm();
+  this.drawEquation();
+  this.mathFormGroup.get('answer').enable(); // make sure the control is re-enabled here
+}
+  
 }
