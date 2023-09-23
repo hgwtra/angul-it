22d6f1f9e835dd24777c246183cdb6107ebec90a
NafiRanta <nafisah.rantasalmi@gmail.com>
2023-09-23 16:02:02 +0300

    shuffle image position

--- src/app/captcha/image/image.component.ts
+++ src/app/captcha/image/image.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./image.component.css']
 })
 export class ImageComponent {
+  imagePaths: string[] = [
+    "./assets/images/answer.jpg",
+    "./assets/images/image2.jpg",
+    "./assets/images/image3.jpg",
+    "./assets/images/image4.jpg",
+    "./assets/images/image5.jpg",
+    "./assets/images/image6.jpg",
+    "./assets/images/image7.jpg",
+    "./assets/images/image8.jpg",
+    "./assets/images/image9.jpg",
+  ];
 
+  constructor() {
+    // Shuffle the imagePaths array randomly
+    this.shuffleArray(this.imagePaths);
+  }
+
+  // Function to shuffle an array randomly
+  private shuffleArray(array: any[]) {
+    let currentIndex = array.length, randomIndex, temporaryValue;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  onImageClicked(imagePath: string) {
+    console.log(imagePath);
+  }
 }
